dc75e32c6f1aca46b0733856773adb704111512b
Criseyda-prog <criseyda.melgar@gmail.com>
2024-12-03 21:16:03 -0600

    Add files via upload

---
+++ portafolio_Crys/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="about">
+            <h2>¿Quien soy?</h2>
+            <p>
+            Soy una aprendiz en programación con conocimientos muy básicos en lenguajes de programación.
+            en mis inicios con C+++ Python y Java, con la ilusión de seguirse superando academicamente no importando la edad. 
+            </p>
+            <p>
+            Graduada de la Universidad de El Salvador y estudiante de la Escuela Superior de Innovación y Tecnología (ESIT), en donde pretendo seguir adquiendo conocimientos.
+            </p>
+        </section>
+    );
+}
+
+export default About;
